66bcef7f94ddf97c43d9c0da22fde1b674b56f05
risaadytiasalma <salma.adytia20@gmail.com>
2026-08-20 10:28:27 +0700

    Update report period from June to July 2026

--- pages/4_Capaian_Penyaluran_TKD.py
+++ pages/4_Capaian_Penyaluran_TKD.py
@@ -80,7 +80,7 @@ st.markdown("<h3 class='sub-header'>Lingkup KPPN Lhokseumawe</h3>", unsafe_allow
 
 # Periode/tanggal
 REPORT_YEAR = 2026
-st.markdown(f"<p class='period'>Periode: Januari - Juni {REPORT_YEAR}</p>", unsafe_allow_html=True)
+st.markdown(f"<p class='period'>Periode: Januari - Juli {REPORT_YEAR}</p>", unsafe_allow_html=True)
 
 
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
@@ -107,6 +107,6 @@ st.markdown('<hr class="divider">', unsafe_allow_html=True)
 # Catatan kaki
 st.caption("""
 **Catatan:**
-- **Sumber:** SINTESA 30 Juni 2026
-- Data yang disajikan adalah per tanggal 30 Juni 2026
+- **Sumber:** SINTESA 31 Juli 2026
+- Data yang disajikan adalah per tanggal 31 Juli 2026
 """)

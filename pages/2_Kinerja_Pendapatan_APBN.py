37bccc4105d7eea1e7fe143f3676784edc4af827
rekonkppn089-max <rekonkppn089@gmail.com>
2026-09-21 09:57:14 +0700

    Update 2_Kinerja_Pendapatan_APBN.py

--- pages/2_Kinerja_Pendapatan_APBN.py
+++ pages/2_Kinerja_Pendapatan_APBN.py
@@ -78,7 +78,7 @@ st.markdown("<h3 class='sub-header'>Lingkup KPPN Lhokseumawe</h3>", unsafe_allow
 
 # Periode/tanggal
 REPORT_YEAR = 2026
-st.markdown(f"<p class='period'>Periode: Januari - Juli {REPORT_YEAR}</p>", unsafe_allow_html=True)
+st.markdown(f"<p class='period'>Periode: Januari - Agustus {REPORT_YEAR}</p>", unsafe_allow_html=True)
 
 
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
@@ -91,7 +91,7 @@ with st.container():
 
 # Analisis tambahan
 #with st.expander("**SUMBER:**", expanded=False):
-    #st.write(""" MPN G3 30 Juni 2025""")
+    #st.write(""" MPN G3 31 Agustus 2026""")
 
 # Tombol kembali ke home
 st.markdown('<div class="back-button">', unsafe_allow_html=True)
@@ -104,6 +104,6 @@ st.markdown('<hr class="divider">', unsafe_allow_html=True)
 # Catatan kaki
 st.caption("""
 **Catatan:**
-- **Sumber:** OMSPAN 31 Juli 2026
-- Data yang disajikan adalah per tanggal 31 Juli 2026
+- **Sumber:** OMSPAN 31 Agustus 2026
+- Data yang disajikan adalah per tanggal 31 Agustus 2026
 """)
